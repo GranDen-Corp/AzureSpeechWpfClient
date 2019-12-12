0a2005f66b0b5c67a6c40815a51b04e7c4eabdc5
Chen Yu Pao <windperson@gmail.com>
2019-12-12 12:03:20 +0800

    Use custom config file path

--- AzureSpeechWpfClient/Services.cs
+++ AzureSpeechWpfClient/Services.cs
@@ -1,11 +1,23 @@
-﻿using System.Windows;
+﻿using System;
+using System.Reflection;
+using System.Windows;
 using Jot;
+using Jot.Storage;
 
 namespace AzureSpeechWpfClient
 {
     static class Services
     {
-        public static Tracker Tracker = new Tracker();
+        public static Tracker Tracker = CreateTracker();
+
+        private static Tracker CreateTracker()
+        {
+            var currentPath = System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+
+            var configFilePath = System.IO.Path.Combine(currentPath, "SpeechSDKConfig");
+
+            return new Tracker(new JsonFileStore(configFilePath));
+        }
 
         static Services()
         {
